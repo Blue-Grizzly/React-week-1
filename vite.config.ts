2ed6776c829754d763776121c6078a65793faa7c
Blue-Grizzly <emilnissen2@gmail.com>
2024-03-01 14:32:00 +0100

    innit commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://blue-grizzly.github.io/React-week-1"
 })
